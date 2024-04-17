4a8f90315f70c92f90a50f2e9aaa1a538f2c8a76
juicyenc <juicyenc@outlook.com>
2024-04-18 04:08:43 +0800

    fix: stablize append_mode test

--- tests/async_file_lock.rs
+++ tests/async_file_lock.rs
@@ -1,16 +1,11 @@
 #![deny(unused_must_use)]
 #![feature(assert_matches)]
 
-use std::io::Result;
-use std::time::Instant;
+use std::path::PathBuf;
 use tempfile::NamedTempFile;
 use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt, SeekFrom};
 use tokio::test;
 use {async_file_lock::FileLock, std::assert_matches::assert_matches};
-use {
-    fork::{fork, Fork},
-    std::path::PathBuf,
-};
 
 fn file() -> FileLock {
     FileLock::new_std(tempfile::tempfile().unwrap())
@@ -94,38 +89,93 @@ async fn normal1() {
     };
 }
 
-#[ignore]
 #[test(flavor = "multi_thread")]
-async fn append_mode() -> Result<()> {
-    let tmp_path = NamedTempFile::new()?.into_temp_path();
-    match fork() {
-        Ok(Fork::Parent(_)) => {
-            let mut buf = String::new();
-            let mut file = FileLock::create(&tmp_path).await?;
-            file.set_seeking_mode(SeekFrom::End(0));
-            file.write(b"a").await?;
-            // println!("parent {}", tmp_path.exists());
-            std::thread::sleep(std::time::Duration::from_millis(200));
-            file.write(b"a").await?;
-            file.seek(SeekFrom::Start(0)).await?;
-            // Turn off auto seeking mode
-            file.set_seeking_mode(SeekFrom::Current(0));
-            file.read_to_string(&mut buf).await?;
-            // Each file handle has its own position.
-            assert_eq!(buf, "bba");
-        }
-        Ok(Fork::Child) => {
-            std::thread::sleep(std::time::Duration::from_millis(100));
-            // println!("{}", tmp_path.exists());
-            let mut file = FileLock::open(&tmp_path).await?;
-            file.write(b"bb").await?;
-            file.flush().await?;
-            // println!("done");
-            std::process::exit(0);
-        }
-        Err(_) => panic!("unable to fork"),
-    }
-    Ok(())
+async fn append_mode() {
+    // let tmp_path = NamedTempFile::new()?.into_temp_path();
+    // match fork() {
+    //     Ok(Fork::Parent(_)) => {
+    //         let mut buf = String::new();
+    //         let mut file = FileLock::create(&tmp_path).await?;
+    //         file.set_seeking_mode(SeekFrom::End(0));
+    //         file.write(b"a").await?;
+    //         // println!("parent {}", tmp_path.exists());
+    //         std::thread::sleep(std::time::Duration::from_millis(200));
+    //         file.write(b"a").await?;
+    //         file.seek(SeekFrom::Start(0)).await?;
+    //         // Turn off auto seeking mode
+    //         file.set_seeking_mode(SeekFrom::Current(0));
+    //         file.read_to_string(&mut buf).await?;
+    //         // Each file handle has its own position.
+    //         assert_eq!(buf, "bba");
+    //     }
+    //     Ok(Fork::Child) => {
+    //         std::thread::sleep(std::time::Duration::from_millis(100));
+    //         // println!("{}", tmp_path.exists());
+    //         let mut file = FileLock::open(&tmp_path).await?;
+    //         file.write(b"bb").await?;
+    //         file.flush().await?;
+    //         // println!("done");
+    //         std::process::exit(0);
+    //     }
+    //     Err(_) => panic!("unable to fork"),
+    // }
+    // Ok(())
+    let file = NamedTempFile::new().unwrap();
+
+    let tmp_path: PathBuf = file.path().into();
+    let tmp_path2 = tmp_path.clone();
+
+    // signal channels
+    let (writen_tx, writen_rx) = tokio::sync::oneshot::channel::<()>();
+    let (overwriten_tx, overwriten_rx) = tokio::sync::oneshot::channel::<()>();
+
+    // child thread
+    tokio::spawn(async move {
+        // open file
+        let mut file = FileLock::open(&tmp_path).await.unwrap();
+        assert!(tmp_path.exists());
+
+        file.set_seeking_mode(SeekFrom::End(0));
+
+        // write to file
+        assert_matches!(file.write(b"a").await, Ok(_));
+        assert_matches!(writen_tx.send(()), Ok(()));
+
+        // wait for overwriten
+        assert_matches!(overwriten_rx.await, Ok(()));
+
+        // write, it should append to the end
+        assert_matches!(file.write(b"a").await, Ok(_));
+
+        assert_matches!(file.seek(SeekFrom::Current(0)).await, Ok(_));
+
+        // turn off auto seeking mode
+        file.set_seeking_mode(SeekFrom::Current(0));
+
+        // read content
+        let mut buf = String::new();
+        assert_matches!(file.read_to_string(&mut buf).await, Ok(_));
+
+        // assert it has been overwriten
+        assert_eq!(buf, "bba");
+    });
+
+    // main thread
+    {
+        // wait for writen signal
+        assert_matches!(writen_rx.await, Ok(()));
+
+        // open file
+        let mut file = FileLock::open(&tmp_path2).await.unwrap();
+        assert!(tmp_path2.exists());
+
+        // overwrite
+        assert_matches!(file.write(b"bb").await, Ok(_));
+        assert_matches!(file.flush().await, Ok(_));
+
+        // signal
+        assert_matches!(overwriten_tx.send(()), Ok(()));
+    };
 }
 
 #[test(flavor = "multi_thread")]
